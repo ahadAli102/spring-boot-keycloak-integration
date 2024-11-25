395f54e848fe15f3a5576ee828a7507800d71907
Ahad Ali <linkonahad10@gmail.com>
2024-11-25 09:12:21 +0600

    adding full code

---
+++ src/test/java/com/ahad/keycloak/integration/SpringBootKeycloakIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahad.keycloak.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
